785e9a5fbbc88133cb80c01da1a9aca9e4163631
U-PS14B22\vantan <akira206@gmail.com>
2015-02-15 17:17:59 +0900

    CharTest

---
+++ CharTest/CharTest.cpp
@@ -0,0 +1,26 @@
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+  /*
+    charは一文字しか保存しないので
+    文字列は×
+  */
+  // char test = "test";
+
+
+  /*
+    文字列を扱うときはポインタにする
+    以下参照(Ctrl + クリック)
+    http://homepage1.nifty.com/toshio-k/prog/c/string.html
+  */
+  // char* test = "test";
+  // cout << test << endl;
+
+	return 0;
+}
+
