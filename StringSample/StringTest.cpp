577f9f34c7e33aa14350613a767e1dc279594218
U-PS14B22\vantan <akira206@gmail.com>
2015-02-15 17:16:36 +0900

    StringSample

---
+++ StringSample/StringTest.cpp
@@ -0,0 +1,27 @@
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+// stringはC++の標準ライブラリなのでインクルード
+#include <string>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+  /*  string型のstrを宣言  */
+  // string str;
+
+
+  /*  string型の初期化方法  */
+  /*
+  string str1 = "test";
+  string str2 = string(10, 'a');
+  string str3(10, 't');
+  cout << str3 << endl;
+  */
+
+
+	return 0;
+}
+
