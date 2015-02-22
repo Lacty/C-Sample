20a9992cd75c1071bb543b378251e0fbd5c703d8
U-PS14B22\vantan <akira206@gmail.com>
2015-02-22 10:47:55 +0900

    とりあえずTextからの読み込み成功

---
+++ Text_Read/main.cpp
@@ -0,0 +1,60 @@
+﻿
+#include "lib/defines.hpp"
+#include "lib/appEnv.hpp"
+#include <fstream>
+
+enum Window {
+  WIDTH  = 512,
+  HEIGHT = 512
+};
+
+
+struct Object{
+  float x;
+  float y;
+  float size_wh;
+  float speed;
+  Color color;
+};
+
+void PlayerMove(AppEnv& env, Object& player){
+  if (env.isPressKey(GLFW_KEY_RIGHT)){
+    player.x += player.speed;
+  }
+  if (env.isPressKey(GLFW_KEY_LEFT)){
+    player.x -= player.speed;
+  }
+  if (env.isPressKey(GLFW_KEY_UP)){
+    player.y += player.speed;
+  }
+  if (env.isPressKey(GLFW_KEY_DOWN)){
+    player.y -= player.speed;
+  }
+}
+
+void PlayerDraw(AppEnv& env, Object& player){
+  drawFillBox(player.x, player.y, player.size_wh, player.size_wh, player.color);
+}
+
+int main() {
+  AppEnv env(Window::WIDTH, Window::HEIGHT, false, false);
+
+  Object player;
+  std::ifstream fstr("res/data.txt");
+  if (fstr){
+    fstr >> player.x >> player.y;
+    fstr >> player.speed;
+    fstr >> player.size_wh;
+    int red, green, blue;
+    fstr >> red >> green >> blue;
+    player.color = Color(red, green, blue);
+  }
+
+  while (env.isOpen()) {
+    PlayerMove(env, player);
+    env.setupDraw();
+    PlayerDraw(env, player);
+
+    env.update();
+  }
+}
