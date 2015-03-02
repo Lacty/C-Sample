3ce270aafdd53a9c9af5b35f2c176d8bc702e48b
U-PS14B22\vantan <akira206@gmail.com>
2015-03-02 17:59:33 +0900

    ひな形

---
+++ LineScroll/main.cpp
@@ -0,0 +1,23 @@
+﻿
+#include "lib/defines.hpp"
+#include "lib/appEnv.hpp"
+
+
+enum Window {
+  WIDTH  = 512,
+  HEIGHT = 512
+};
+
+
+
+int main() {
+  AppEnv env(Window::WIDTH, Window::HEIGHT, false, false);
+
+  while (env.isOpen()) {
+    env.setupDraw();
+
+    
+
+    env.update();
+  }
+}
