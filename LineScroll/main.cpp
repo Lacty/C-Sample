4cfe4e423a183626c4f15ec05ac11fa1a597303b
U-PS14B22\vantan <akira206@gmail.com>
2015-03-02 18:14:04 +0900

    画像をsin波で左右にアニメーションさせる

--- LineScroll/main.cpp
+++ LineScroll/main.cpp
@@ -27,10 +27,16 @@ int main() {
   Texture miku_image("res/miku.png");
   Object miku(-256/2, -256/2, 256, 256);
 
+  float angle = 0;
+  float angle_x = 0;
+
   while (env.isOpen()) {
     env.setupDraw();
 
-    drawTextureBox(miku.x, miku.y, miku.width, miku.height,
+    angle += 0.1;
+    angle_x = std::sin(angle) * 10;
+
+    drawTextureBox(miku.x + angle_x, miku.y, miku.width, miku.height,
                    0, 0, miku.width, miku.height,
                    miku_image,
                    Color(1, 1, 1));
