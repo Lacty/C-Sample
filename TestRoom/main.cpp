d7490aae106fc630d1dc463214cff7f204109e30
U-PS14B22\vantan <akira206@gmail.com>
2015-03-05 18:55:04 +0900

    max, min

--- TestRoom/main.cpp
+++ TestRoom/main.cpp
@@ -1,34 +1,36 @@
 
 #include <iostream>
+#include <algorithm>
 
-
-class Fuga{
-public:
-  int x;
-  int y;
-
-  // 初期化リストを使えば”宣言時”に値を入れることができる
-  // コンストラクタ本体の処理({}の中)だと、宣言後に”代入”することとなる
-  Fuga() : x(1), y(2)
-  {}
-};
-
-// Fugaクラスを丸ごとコピーして生成することになるので処理が重い
-int Func1(Fuga fuga){
-  return fuga.x + fuga.y;
-}
-
-// constは値を変更しないと明示し保障
-int Func2(const Fuga& fuga){
-  return fuga.x + fuga.y;
-}
 
 int main()
 {
-  Fuga fuga;
+  // 1, 0 どちらが大きいか判断して、大きい方を出力
+  std::cout << "max = " << std::max(1, 0) << std::endl;
 
-  std::cout << "Func1 = " << Func1(fuga) << std::endl; // 3
-  std::cout << "Func2 = " << Func2(fuga) << std::endl; // 3
+  // 1, 0 どちらが小さいか判断して、小さい方を出力
+  std::cout << "min = " << std::min(1, 0) << std::endl;
+
+  
+  //----------ゲームでの使用例------------//
+  enum Window{
+    WIDTH  = 200
+  };
+
+  struct Player{
+    float x;
+    float speed;
+    Player() : x(205.0f), speed(5.0f){}
+  }player;
+
+  int sign = -1;
+
+  if ((player.x >=  WIDTH * 0.5f) ||
+      (player.x <= -WIDTH * 0.5f)){
+    player.speed *= sign;
+    player.x = std::max(player.x, -WIDTH * 0.5f);
+    player.x = std::min(player.x,  WIDTH * 0.5f);
+  }
 
   return 0;
 }
