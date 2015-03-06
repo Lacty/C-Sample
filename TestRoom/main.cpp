f774d59119473b661c7c8cac456d027b8633412b
U-PS14B22\vantan <akira206@gmail.com>
2015-03-06 14:00:04 +0900

    テンプレート関数

--- TestRoom/main.cpp
+++ TestRoom/main.cpp
@@ -1,37 +1,19 @@
 
 #include <iostream>
-#include <vector>
 
+// 貰った引数をまた戻すテンプレート関数
+template<typename T> // T:なんでもOKな型
+T Func(const T& x){
+  return x;
+}
 
 int main()
 {
-  /* 繰り返し処理(iteration) */
-  int ary[5] = { 1, 2, 3, 4, 5 };
-  for (int i = 0; i < 5; ++i){
-    std::cout << ary[i] << std::endl;
-  }
+  int num = 3;
 
-  /* ポインタを使った繰り返し処理 */
-  for (int* it = ary; it < &ary[5]; ++it){
-    std::cout << *it << std::endl;
-  }
+  auto i = Func<int>(num);
 
-  /* イテレータを使った繰り返し処理 */
-  std::vector<int> v = { 1, 2, 3, 4, 5 };
-  for (std::vector<int>::iterator it = v.begin(); it != v.end(); ++it){
-    std::cout << *it << std::endl;
-  }
-  /*
-   * it < v.end()　では無いのは
-   * イテレーターが<演算子で比較できるとは限らないから
-  */
-
-
-  /* 長ったらしいのでC++11のfor rangeを使う */
-  // ポインタ(*it)でなくてOK
-  for (auto it : v){
-    std::cout << it << std::endl;
-  }
+  std::cout << "i = " << i << std::endl;
 
   return 0;
 }
