3d705a6e8e25613fc10d2af093515cd3168d7921
U-PS14B22\vantan <akira206@gmail.com>
2015-03-06 18:58:07 +0900

    三角形を回転させるサンプル

---
+++ HadeSample/TriangleAngle.cpp
@@ -0,0 +1,31 @@
+﻿
+#include "lib/defines.hpp"
+#include "lib/appEnv.hpp"
+
+enum Window {
+  WIDTH = 512,
+  HEIGHT = 512
+};
+
+
+int main()
+{
+  AppEnv env(Window::WIDTH, Window::HEIGHT, false, false);
+
+  float angle = 0;
+
+  while (env.isOpen()){
+    env.setupDraw();
+
+    angle -= 0.05f;
+
+    drawFillCircle(0, 0, 40, 40, 3,
+      Color(1, 1, 1),
+      angle,
+      Vec2f(1, 1),
+      Vec2f(0, 0)); // Vec2f(0, 50);にすると変化がわかる
+
+
+    env.update();
+  }
+}
