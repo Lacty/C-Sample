e0065feed317471203e0d6680bb1a20fce2bfced
U-PS14B22\vantan <akira206@gmail.com>
2015-03-19 02:53:51 +0900

    Component失敗

--- TestRoom/main.cpp
+++ TestRoom/main.cpp
@@ -1,19 +1,91 @@
 
 #include <iostream>
 
-// 貰った引数をまた戻すテンプレート関数
-template<typename T> // T:なんでもOKな型
-T Func(const T& x){
-  return x;
-}
 
-int main()
-{
-  int num = 3;
+class cObjectAtk;
+class cObjectMove;
+class cObjectInterface;
 
-  auto i = Func<int>(num);
+class cObject{
+private:
+  cObjectAtk* m_atk;
+  cObjectMove* m_move;
 
-  std::cout << "i = " << i << std::endl;
+public:
+  cObject(){
+    std::cout << "キャラクター登場" << std::endl;
+  }
+
+  ~cObject(){
+    if (m_atk != nullptr)
+      delete m_atk;
+
+    if (m_move != nullptr)
+      delete m_move;
+
+    std::cout << std::endl;
+    std::cout << "キャラクター退場" << std::endl;
+  }
+
+  void componentInit(){
+    if (m_atk == nullptr)
+      m_atk = new cObjectAtk(this);
+
+    if (m_move == nullptr)
+      m_move = new cObjectMove(this);
+  }
+
+};
+
+class cObjectInterface{
+protected:
+  cObject* m_obj;
+
+  cObjectInterface(cObject* obj) :
+    m_obj(obj){
+    std::cout << "機能を展開" << std::endl;
+  }
+  ~cObjectInterface(){
+    std::cout << "機能を停止" << std::endl;
+  }
+};
+
+class cObjectAtk : public cObjectInterface{
+private:
+  int m_atk_pow;
+
+public:
+  cObjectAtk(cObject* obj) :
+    cObjectInterface(obj),
+    m_atk_pow(0){
+    std::cout << "攻撃機能を展開" << std::endl;
+  }
+
+  ~cObjectAtk(){
+    std::cout << "攻撃機能を停止" << std::endl;
+  }
+
+};
+
+class cObjectMove : public cObjectInterface{
+private:
+  int m_pos_x;
+
+public:
+  cObjectMove(cObject* obj) :
+    cObjectInterface(obj),
+    m_pos_x(0){
+    std::cout << "移動機能を展開" << std::endl;
+  }
+  ~cObjectMove(){
+    std::cout << "移動機能を停止" << std::endl;
+  }
+
+};
+
+int main(){
+  cObject obj;
+  obj.componentInit();
 
   return 0;
 }
