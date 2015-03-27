e978cfaa1051aa80fb0c77868c8eeeec52db3ebe
U-PS14B22\vantan <akira206@gmail.com>
2015-03-27 11:18:37 +0900

    data_loaderをGameSceneで使用（データ用意してないからエラー吐くけど

--- GameFramework.ver2/Scene/game_scene.cpp
+++ GameFramework.ver2/Scene/game_scene.cpp
@@ -3,13 +3,37 @@
 
 
 cGameScene::cGameScene(cSceneManager* manager) :
-cScene(manager) {}
+cScene(manager),
+m_fhase_num(First_Fhase),
+m_font("res/mplus12r.ttf") {}
 
 
 void cGameScene::update(){
-  
+  if (env::get()->isPushButton(Mouse::LEFT)){
+    enemyUpdate();
+  }
+
+  if (isGameClear()){
+    m_manager->shiftNextScene(std::make_shared<cTitle>(m_manager));
+  }
 }
 
 void cGameScene::draw(){
-  drawFillCircle(0, 0, 50, 50, 4, Color(1, 1, 1));
+  drawFillCircle(0, 0, 50, 50, m_fhase_num, Color(1, 0, 1));
+
+  const float info_x = WindowSize::WIDTH / 2;
+  const float info_y = WindowSize::HEIGHT / 2;
+  // m_font.draw(m_enemy, Vec2f(info_x, info_y), Color::white);
+}
+
+void cGameScene::enemyUpdate(){
+  ++m_fhase_num;
+
+  if (isGameClear()) return;
+
+  m_enemy = m_loader.output(m_fhase_num);
+}
+
+bool cGameScene::isGameClear(){
+  return m_fhase_num > Last_Fhase;
 }
--- GameFramework.ver2/Scene/game_scene.h
+++ GameFramework.ver2/Scene/game_scene.h
@@ -2,6 +2,7 @@
 #pragma once
 #include "../System/environment.h"
 #include "../System/scene_manager.h"
+#include "../System/data_loader.hpp"
 #include "scene.h"
 
 
@@ -12,4 +13,20 @@ public:
 
   void update();
   void draw();
+
+
+private:
+
+  enum Fhase {
+    First_Fhase = 1,
+    Last_Fhase  = 5
+  };
+  void enemyUpdate();
+  bool isGameClear();
+
+  int m_fhase_num;
+  std::string m_enemy;
+  cDataLoader m_loader;
+
+  Font m_font;
 };
