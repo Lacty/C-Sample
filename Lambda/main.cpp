a95b8478e35e10ab585cd2461432e91f788431df
U-PS14B22\vantan <akira206@gmail.com>
2015-03-28 19:54:54 +0900

    コピー、参照キャプチャやってみた

--- Lambda/main.cpp
+++ Lambda/main.cpp
@@ -2,37 +2,54 @@
 #include <iostream>
 
 
-// 引数に与えた数値が2倍になって帰ってくる関数
-int hoge(int& val){
-  return val * 2;
-}
-
 int main(){
   // ラムダ式の構造
   // [キャプチャ](仮引数)->戻り値{複合分文};
 
-  // ラムダ式hoge関数
-  auto piyo = [](int& val)->int{
-    return val * 2;
-  };
-
+  // [=]:コピーキャプチャ
+  
   {
-    // hoge
-    int val = 2;
-    std::cout << hoge(val) << std::endl;
-    
-    // 出力結果
-    // 4
+    int cpy = 5;
+    auto hoge = [=](int val)->int{
+      return cpy + val;
+    };
+    std::cout << hoge(10) << std::endl;
   }
 
-  {
-    // piyo
-    int val = 3;
-    std::cout << piyo(val) << std::endl;
+  /*{
+    int cpy = 5;
+    auto hoge = [](int val)->int{
 
-    // 出力結果
-    // 6
+      return cpy + val;
+
+      // cpyとはなんぞや？
+      // と怒られる
+    };
+    std::cout << hoge(10) << std::endl;
+  }*/
+
+  {
+    // 個別に指定することも可能
+    int cpy1 = 5;
+    int cpy2 = 20;
+    auto hoge = [cpy1, cpy2](int val)->int{
+      return cpy1 + cpy2 + val;
+    };
+    std::cout << hoge(10) << std::endl;
   }
+
+  /*{
+    int cpy1 = 5;
+    int cpy2 = 20;
+    auto hoge = [cpy1](int val)->int{
+
+      return cpy1 + cpy2 + val;
+      
+      // cpy2とはなんぞや？
+      // と怒られる
+    };
+    std::cout << hoge(10) << std::endl;
+  }*/
 
   return 0;
 }
