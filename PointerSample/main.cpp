f63cabdf6ad959b94766611b33d1eab905489b2d
U-PS14B22\vantan <akira206@gmail.com>
2015-04-11 19:31:46 +0900

    配列のポインタ渡し

--- PointerSample/main.cpp
+++ PointerSample/main.cpp
@@ -2,59 +2,36 @@
 #include <iostream>
 
 
+class Test01 {
+private:
+  float x[10];
+
+public:
+  Test01() {
+    for (int i = 0; i < 10; ++i) 
+    {
+      x[i] = i;
+    }
+  }
+
+  float* getX() {
+    return x;
+  }
+};
+
+class Test02 {
+public:
+  void Show(float* x) {
+    std::cout << x[0] << std::endl;
+  }
+};
+
 int main()
 {
-  int   iValue = 5;           // int型
-  int*  piValue = &iValue;    // piValueにiValueのアドレスを代入
+  Test01 test01;
+  Test02 test02;
 
-  // 5
-  std::cout << "iValue = " << iValue << std::endl;
-
-  // iValueのアドレス
-  std::cout << "&iValue = " << &iValue << std::endl;
-
-  // piValueはiValueのアドレスを見ているので
-  // iValueのアドレス
-  std::cout << "piValue = " << piValue << std::endl;
-
-  // piValueのアドレス
-  std::cout << "&piValue = " << &piValue << std::endl;
-
-  // ポインタを使いiValueの中身を参照
-  // 5
-  std::cout << "*piValue = " << *piValue << std::endl;
-
-  
-
-  struct Player{
-    int x;
-    int y;
-    Player(int x, int y){
-      this->x = x;
-      this->y = y;
-    }
-  };
-
-  Player player(1, 2);           // x=1, y=2で初期化
-  Player* pplayer = &player;     // pplayerにplayerのアドレスを代入
-
-  // ちゃんと同じアドレスを吐き出すか確認
-  // 結果：同じアドレスを吐き出した
-  std::cout << "&player = " << &player << std::endl;
-  std::cout << "pplayer = " << pplayer << std::endl;
-
-  // player.xのアドレス
-  // 結果：playerのアドレスを吐き出した
-  std::cout << "&player.x = " << &player.x << std::endl;
-
-  // player.yのアドレス
-  // 結果：playerのアドレスを吐き出した
-  std::cout << "&player.y = " << &player.y << std::endl;
-
-  /*
-   * pplayer.xはできない
-   * あくまでplayerのアドレスを持つ
-  */
+  test02.Show(test01.getX());
 
   return 0;
 }
