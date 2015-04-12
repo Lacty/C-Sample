554c459dc2f4a9d6dcb31308cdf8b4b642ea6078
U-PS14B22\vantan <akira206@gmail.com>
2015-04-13 00:54:23 +0900

    MapChit新規

---
+++ MapChip/main.cpp
@@ -0,0 +1,31 @@
+﻿
+#include "lib/framework.hpp"
+
+enum WindowSize {
+  WIDTH  = 512,
+  HEIGHT = 512,
+};
+
+
+int main() {
+  AppEnv env(WIDTH, HEIGHT);
+
+  enum MapArray {
+    Map_Y = 5,
+    Map_X = 5
+  };
+  int map[Map_Y][Map_X] =
+  {
+    { 0, 0, 0, 0, 0 },
+    { 1, 1, 1, 1, 1 },
+    { 2, 2, 2, 2, 2 },
+    { 3, 3, 3, 3, 3 },
+    { 4, 4, 4, 4, 4 }
+  };
+
+  while (env.isOpen()) {
+    env.begin();
+
+    env.end();
+  }
+}
