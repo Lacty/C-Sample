d17c1e544ec0a38135c8e197b771ed513807bd1c
U-PS14B22\vantan <akira206@gmail.com>
2015-04-28 20:58:41 +0900

    Mouse_Box

---
+++ PS15/Mouse_Box/main.cpp
@@ -0,0 +1,24 @@
+﻿
+#include "lib/framework.hpp"
+
+
+int main() {
+  AppEnv env(600, 400);
+
+  Color color(1, 0, 0);
+
+  while (env.isOpen()) {
+    env.begin();
+
+    Vec2f pos = env.mousePosition();
+
+    if (env.isPressButton(Mouse::LEFT)) {
+      color = Color(1, 1, 1);
+    } else {
+      color = Color(1, 0, 0);
+    }
+      drawFillBox(pos.x() - 25, pos.y() - 25,
+                  50, 50, color);
+    env.end();
+  }
+}
