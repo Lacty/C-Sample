fa8bc74524c19688ccdf0763c3d4a58bd73f5c8a
U-PS14B22\vantan <akira206@gmail.com>
2015-06-15 19:48:35 +0900

    修正

--- _6_15/main.cpp
+++ _6_15/main.cpp
@@ -10,31 +10,28 @@ int main() {
   AppEnv env(640, 480);
 
   Player player;
-  //Enemy enemy;
   std::list<Enemy> enemy;
   enemy.emplace_back(Vec2f(0, 0));
-  enemy.emplace_back(Vec2f(-100, 0));
-  enemy.emplace_back(Vec2f(100, 0));
+  enemy.emplace_back(Vec2f(-150, 0));
+  enemy.emplace_back(Vec2f(150, 0));
 
   while (env.isOpen()) {
     for (auto it : enemy) {
       it.update();
     }
-    //enemy.update();
     player.update(env);
 
+    player.setColor(Color::white);
     for (auto it : enemy) {
       if (isCollisionRectToRect(player.getPos(), player.getSize(),
-                                it.getPos(), it.getSize())) {
+                                it.getPos(), it.getSize()))
+      {
         player.setColor(Color::red);
-      } else {
-        player.setColor(Color::white);
       }
     }
 
     env.begin();
 
-    //enemy.draw();
     for (auto it : enemy) {
       it.draw();
     }
